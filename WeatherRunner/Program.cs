7a116c75626fcd57f2548635b84066f4e1325d17
Richard Hibbitts <rhibbitts@technopoetic.com>
2019-09-02 13:48:40 -0400

    Console App

---
+++ WeatherRunner/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Weather;
+
+namespace WeatherRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Loading...");
+            // var _filename = "weather_data.csv"
+            var _weatherService = new WeatherService();
+
+            while(true){
+                Console.WriteLine("Enter a date to view the weather for that day.");
+                var enteredDate = Console.ReadLine();
+                Console.WriteLine($"You entered {enteredDate}.");
+
+                Console.WriteLine(_weatherService.Weather);
+            }
+        }
+
+    }
+}
