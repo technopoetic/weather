a84ef507d5c003c12922c8b0833d848b252b33bc
Richard Hibbitts <rhibbitts@technopoetic.com>
2019-09-02 13:48:05 -0400

    WeatherService Class lib

---
+++ WeatherService/WeatherService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Weather
+{
+    public class WeatherService
+    {
+        public string Weather;
+        public WeatherService()
+        {
+            this.Weather = "sunny";
+        }
+    }
+}
