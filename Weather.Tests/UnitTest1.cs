96d68a0729b1fd34062014404d5ad308f3fb656c
Richard Hibbitts <rhibbitts@technopoetic.com>
2019-09-03 06:35:33 -0400

    Update unit tests

--- Weather.Tests/UnitTest1.cs
+++ Weather.Tests/UnitTest1.cs
@@ -10,12 +10,24 @@ namespace Weather.Tests
         public UnitTest1()
         {
             _weatherService = new WeatherService();
+            
         }
 
         [Fact]
-        public void Test1()
+        public void TestGetWeather()
         {
-            Assert.Equal(_weatherService.Weather, "sunny");
+            // This data should probably be mocked.
+            var weather = _weatherService.GetWeather(DateTime.Parse("12/20/2019"));
+            Assert.Equal(weather.NormalMaxTemp, 52.2);
+            Assert.Equal(weather.NormalMinTemp, 32.3);
+            Assert.Equal(weather.NormalPrecipitation, 0.1);
+        }
+
+        [Fact]
+        public void TestBuildKey()
+        {
+            // Need to do some more research on testing.  I haven't been able to figure out how to test private methods.
+            Assert.Equal(1, 1);
         }
     }
 }
